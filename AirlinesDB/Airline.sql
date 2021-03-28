3c1a0b6e4f6ee25fe765776eb120c011fe17ab97
Dhruv96 <dhruvgrover111@gmail.com>
2021-03-28 14:54:51 -0700

    Added database project

---
+++ AirlinesDB/Airline.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Airline]
+(
+	[AirlineId] INT NOT NULL, 
+    [AirlineName] NCHAR(10) NOT NULL
+	CONSTRAINT PK_AIRLINE PRIMARY KEY (AirlineId)
+);
---
+++ AirlinesDB/Flight.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Flight]
+(
+	[FlightId] INT NOT NULL, 
+    [AirlineId] INT NOT NULL, 
+    [DepartureAirport] INT NOT NULL, 
+    [DestinationAirport] INT NOT NULL, 
+    [FlightDate] DATETIME NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [NumSeats] INT NOT NULL,
+    CONSTRAINT PK_FLIGHT PRIMARY KEY ([FlightId])
+);
