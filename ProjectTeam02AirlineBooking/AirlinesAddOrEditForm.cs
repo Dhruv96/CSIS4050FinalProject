6c7b88d4a40cde90ff3e408f65b29754592c7a3f
jbh0630 <69543299+jbh0630@users.noreply.github.com>
2021-04-05 01:02:46 -0700

    admin page, booking manage page

---
+++ ProjectTeam02AirlineBooking/AirlinesAddOrEditForm.cs
@@ -0,0 +1,106 @@
+﻿using AirlineBookingCodeFirstFromDB;
+using EFControllerUtilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectTeam02AirlineBooking
+{
+    public partial class AirlinesAddOrEditForm : Form
+    {
+        AirlineEntities context;
+        public AirlinesAddOrEditForm()
+        {
+            InitializeComponent();
+
+            this.Load += AirlinesAddOrEditForm_Load;
+
+            listBoxAirlinesManageList.SelectedIndexChanged += (s, e) => GetAirlines();
+            buttonAirlineManageAdd.Click += ButtonAirlineManageAdd_Click;
+            buttonAirlineManageUpdate.Click += ButtonAirlineManageUpdate_Click;
+
+        }
+
+        private void ButtonAirlineManageUpdate_Click(object sender, EventArgs e)
+        {
+            if (!(listBoxAirlinesManageList.SelectedItem is Airline airline)) 
+            {
+                MessageBox.Show("Airline to be updated must be selected");
+                return;
+            }
+
+            airline.AirlineId = int.Parse(textBoxAirlineIdManage.Text);
+            airline.AirlineName = textBoxAirlineNameManage.Text;
+
+            if (airline.AirlineId != null && airline.AirlineName != null
+                && airline.AirlineId.ToString().Trim().Length != 0 &&
+                airline.AirlineName.Trim().Length != 0)
+            {
+                if (Controller<AirlineEntities, Airline>.UpdateEntity(airline) == false)
+                {
+                    MessageBox.Show("Cannot update airline to database");
+                    return;
+                }
+                this.DialogResult = DialogResult.OK;
+                Close();
+            }
+            else 
+            {
+                MessageBox.Show("Airline information is missing");
+                return;
+            }
+
+        }
+
+        private void ButtonAirlineManageAdd_Click(object sender, EventArgs e)
+        {
+            Airline airline = new Airline();
+            airline.AirlineId = int.Parse(textBoxAirlineIdManage.Text);
+            airline.AirlineName = textBoxAirlineNameManage.Text;
+
+            if (airline.AirlineId != null && airline.AirlineName != null &&
+                airline.AirlineId.ToString().Trim().Length != 0 && airline.AirlineName.Trim().Length != 0)
+            {
+                if (Controller<AirlineEntities, Airline>.AddEntity(airline) == null)
+                {
+                    MessageBox.Show("Cannot add airline to database");
+                    return;
+                }
+                this.DialogResult = DialogResult.OK;
+                Close();
+            }
+            else 
+            {
+                MessageBox.Show("Please enter all the details");
+            }
+                
+        }
+
+        private void AirlinesAddOrEditForm_Load(object sender, EventArgs e)
+        {
+            context = new AirlineEntities();
+            listBoxAirlinesManageList.DataSource = Controller<AirlineEntities, Airline>.SetBindingList();
+            textBoxAirlineIdManage.ResetText();
+            textBoxAirlineNameManage.ResetText();
+            listBoxAirlinesManageList.SelectedIndex = -1;
+        }
+
+        private void GetAirlines() 
+        {
+            if (!(listBoxAirlinesManageList.SelectedItem is Airline airline))
+                return;
+
+            textBoxAirlineIdManage.Text = airline.AirlineId.ToString();
+            textBoxAirlineNameManage.Text = airline.AirlineName;
+
+          
+        }
+
+    }
+}
---
+++ ProjectTeam02AirlineBooking/ManageFlightsForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace ProjectTeam02AirlineBooking
+{
+    partial class ManageFlightsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFlightsInfo = new System.Windows.Forms.Button();
+            this.buttonAirlinesInfo = new System.Windows.Forms.Button();
+            this.buttonAirportsInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFlightsInfo
+            // 
+            this.buttonFlightsInfo.Location = new System.Drawing.Point(110, 54);
+            this.buttonFlightsInfo.Name = "buttonFlightsInfo";
+            this.buttonFlightsInfo.Size = new System.Drawing.Size(75, 23);
+            this.buttonFlightsInfo.TabIndex = 0;
+            this.buttonFlightsInfo.Text = "Flights Info";
+            this.buttonFlightsInfo.UseVisualStyleBackColor = true;
+            // 
+            // buttonAirlinesInfo
+            // 
+            this.buttonAirlinesInfo.Location = new System.Drawing.Point(110, 120);
+            this.buttonAirlinesInfo.Name = "buttonAirlinesInfo";
+            this.buttonAirlinesInfo.Size = new System.Drawing.Size(75, 23);
+            this.buttonAirlinesInfo.TabIndex = 1;
+            this.buttonAirlinesInfo.Text = "Airlines Info";
+            this.buttonAirlinesInfo.UseVisualStyleBackColor = true;
+            // 
+            // buttonAirportsInfo
+            // 
+            this.buttonAirportsInfo.Location = new System.Drawing.Point(110, 199);
+            this.buttonAirportsInfo.Name = "buttonAirportsInfo";
+            this.buttonAirportsInfo.Size = new System.Drawing.Size(75, 23);
+            this.buttonAirportsInfo.TabIndex = 2;
+            this.buttonAirportsInfo.Text = "Airports Info";
+            this.buttonAirportsInfo.UseVisualStyleBackColor = true;
+            // 
+            // ManageFlightsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 305);
+            this.Controls.Add(this.buttonAirportsInfo);
+            this.Controls.Add(this.buttonAirlinesInfo);
+            this.Controls.Add(this.buttonFlightsInfo);
+            this.Name = "ManageFlightsForm";
+            this.Text = "ManageFlightsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFlightsInfo;
+        private System.Windows.Forms.Button buttonAirlinesInfo;
+        private System.Windows.Forms.Button buttonAirportsInfo;
+    }
+}
